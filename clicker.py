e526f8423a6a9cd783839c4e5d1e36b186c4b412
Clonethis <jirka.brtnik@post.cz>
2021-12-12 10:14:33 +0100

    Readme

--- clicker.py
+++ clicker.py
@@ -7,7 +7,13 @@
 
 # text input end with 0 if 0 check for another shortcut to record
 # import pyautogui as pt;
+# docs : https://pynput.readthedocs.io/en/latest/index.html
 from pynput import mouse, keyboard
+
+# docs:https://docs.python.org/3/library/sys.html#sys.platform
+import sys
+
+# import platform
 from pynput.mouse import Button, Controller
 
 # from pynput.keyboard import Key, Controller
@@ -32,6 +38,7 @@ tutorial = ["Press keys in :'' to make routine","'t' -> records text",
 "'r' -> right mouse click"
 "'d' -> deletes last command",
 "'n' -> closes and saves data",
+"'l' -> for write out all comands in this session",
 "Program ends with mouse cursor on top left corner or by pressing: 'q'"
 ]
 # mouse.position=(100,100)
@@ -39,7 +46,10 @@ tutorial = ["Press keys in :'' to make routine","'t' -> records text",
 
 # print(tutorial)
 userInput = ""
-userCreatingData=[]
+# dict docs: https://www.w3schools.com/python/python_dictionaries.asp
+# inputDict ={"s":damn(),"v":funct()}
+userCreatingData=[()]
+# userCreatingData[2,{"s"}]
 # class KeyboardPress:
 #     def __init__(command,value):
 #        command = ""
@@ -47,6 +57,12 @@ userCreatingData=[]
 # key = KeyboardPress()
 
 def firstLayout():
+    # print(os.name())
+    for i in sys.argv:
+        print("system platform: " +i)
+    # for i in platform.mac_ver():
+    #     print(i)
+    # print("source architecture: "+platform.mac_ver())
     userInput = input("Press 'c' for create new routine or 'l' for load last routine\n")
     if(userInput=='c'):
         for i in tutorial:
@@ -56,12 +72,21 @@ def firstLayout():
             userInput= input("Press key:(t,s,d,m,r,n)")
             print("user Input2 after : ",userInput)
             if userInput=="r":
-                mouse.press(Button.right)
-                mouse.release(Button.right)
-                userCreatingData.append("r")
+                # mouse.press(Button.right)
+                # mouse.release(Button.right)
+                pos = mouse.position
+                userCreatingData.append(("r",pos))
+                for i in userCreatingData:
+                    print(f"userArray: {i}")
+                # userCreatingData.append(["r"])
+# generator docs; https://www.python.org/dev/peps/pep-0289/
+                
 
-                print("Iran")
-                sleep(0.1)
+                
+                # print(pos)
+                # userCreatingData.append(["r",pos])
+                print("Right mouse click")
+                # sleep(0.1)
             elif userInput=="c":
                 for i in userCreatingData:
                     print(i)
@@ -75,12 +100,20 @@ def firstLayout():
             print("Try creating option first ")
             firstLayout()
         else:
-            print(os.listdir("saves"))
-            whichOne=input("\nwhich should I run\n")
+            # print("Saved: '")
+            print("\nWhich number would you like to run: ")
+            for x, i in enumerate(os.listdir("saves")):
+                i=i.split(".pkl")
+                print(f"{x}: {i[0]}")
+            # print("'")
+            whichOne=input("")
             os.chdir("saves")        
             
             load.loadMode 
-
+    else: 
+        print("Try valid char: 'c' or 'l' ")
+        firstLayout()
+        
 # while(mouse.position!=(0,0)):
 #     print('The current pointer position is %s' % format(mouse.position) )
 #     sleep(0.3)
